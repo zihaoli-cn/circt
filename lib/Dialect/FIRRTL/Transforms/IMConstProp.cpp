0b587294c4f9bb0f3f7e91c0bd0c9ae462f2a63f
Hideto Ueno <uenoku.tokotoko@gmail.com>
2021-12-02 21:16:04 +0900

    Remove a redundant condition and add assert

--- lib/Dialect/FIRRTL/Transforms/IMConstProp.cpp
+++ lib/Dialect/FIRRTL/Transforms/IMConstProp.cpp
@@ -748,9 +748,7 @@ void IMConstPropPass::markWireOrUnresetableRegOp(Operation *wireOrReg) {
 }
 
 void IMConstPropPass::markRegResetOp(RegResetOp regReset) {
-  if (!isTrackableType(regReset.getType()))
-    return markOverdefined(regReset);
-
+  assert(isTrackableType(regReset.getType()) && "register is always trackable");
   // The reset value may be known - if so, merge it in.
   auto &destTypes = getLeafGroundTypes(regReset.getType());
 
@@ -1152,12 +1150,8 @@ void IMConstPropPass::rewriteModuleBody(FModuleOp module) {
     // If the op had any constants folded, replace them.
     builder.setInsertionPoint(&op);
     bool foldedAny = false;
-    for (auto result : op.getResults()) {
-      LLVM_DEBUG(llvm::dbgs() << "Trying to replace " << result);
-      bool successReplace = replaceValueIfPossible(result);
-      LLVM_DEBUG(llvm::dbgs() << "success= " << successReplace << "\n");
-      foldedAny |= successReplace;
-    }
+    for (auto result : op.getResults())
+      foldedAny |= replaceValueIfPossible(result);
 
     if (foldedAny)
       ++numFoldedOp;
