8a3bfe275de73c62c9d04bf4de60f5555f6a4570
Hideto Ueno <uenoku.tokotoko@gmail.com>
2022-01-23 07:28:11 +0900

    [HWCleanUp] Aggressive if conditions hoisting

--- lib/Dialect/SV/Transforms/HWCleanup.cpp
+++ lib/Dialect/SV/Transforms/HWCleanup.cpp
@@ -14,8 +14,11 @@
 //===----------------------------------------------------------------------===//
 
 #include "PassDetail.h"
+#include "circt/Dialect/Comb/CombOps.h"
 #include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/SV/SVPasses.h"
+#include "llvm/ADT/SetVector.h"
+#include "llvm/Support/Debug.h"
 
 using namespace circt;
 
@@ -87,7 +90,70 @@ static void mergeRegions(Region *region1, Region *region2) {
   }
 }
 
+// This struct analyses given two conditions.
+// cond1 = conj1 /\ commonConj
+// cond2 = conj2 /\ commonConj
+struct ConditionPairInformation {
+  ConditionPairInformation(Value cond1, Value cond2) {
+    peelConjunction(cond1, conj1);
+    peelConjunction(cond2, conj2);
+
+    for (auto c1 : conj1)
+      if (conj2.contains(c1))
+        commonConj.push_back(c1);
+
+    if (commonConj.empty())
+      return;
+
+    // If conj1 is equal to conj2, all conditions are common.
+    if (commonConj.size() == conj1.size() &&
+        commonConj.size() == conj2.size()) {
+      conj1.clear();
+      conj2.clear();
+      return;
+    }
+
+    // Remove all common values from both sets.
+    for (auto c : commonConj) {
+      conj1.remove(c);
+      conj2.remove(c);
+    }
+  }
+
+  void cleanUpUses() {
+    for (Operation *op : andOperations)
+      if (op->use_empty())
+        op->erase();
+  }
+
+  // Set to store unique values in the first condition.
+  llvm::SmallSetVector<Value, 4> conj1;
+  // Set to store unique values in the second condition.
+  llvm::SmallSetVector<Value, 4> conj2;
+  // Store common values of two conditions.
+  llvm::SmallVector<Value> commonConj;
+
+private:
+  // Track decomposed operations which might be dead after if-merging.
+  llvm::SmallSetVector<comb::AndOp, 4> andOperations;
+
+  // Helper function to accmulate conditions.
+  void peelConjunction(Value cond,
+                       llvm::SmallSetVector<Value, 4> &conjunctions) {
+    auto andOp = dyn_cast_or_null<comb::AndOp>(cond.getDefiningOp());
+    if (!andOp) {
+      conjunctions.insert(cond);
+      return;
+    }
+
+    andOperations.insert(andOp);
+    llvm::for_each(andOp.getOperands(),
+                   [&](Value cond) { peelConjunction(cond, conjunctions); });
+  }
+};
+
 //===----------------------------------------------------------------------===//
+
 // HWCleanupPass
 //===----------------------------------------------------------------------===//
 
@@ -109,6 +175,25 @@ private:
 
     op2->erase();
     anythingChanged = true;
+  }
+
+  sv::IfOp mergeIfOpConditions(sv::IfOp op1, sv::IfOp op2);
+
+  sv::IfOp mergeIfOps(sv::IfOp ifOp, sv::IfOp prevIfOp) {
+    assert(ifOp != prevIfOp && "Cannot merge an op into itself");
+
+    // If conditions of op1 and op2 are equal, we can just merge them.
+    if (ifOp.cond() == prevIfOp.cond()) {
+      anythingChanged = true;
+      mergeOperationsIntoFrom(ifOp, prevIfOp);
+      return ifOp;
+    }
+
+    // TOOD: Handle even when they have else blocks
+    if (ifOp.hasElse() || prevIfOp.hasElse())
+      return ifOp;
+
+    return mergeIfOpConditions(ifOp, prevIfOp);
   }
 
   bool anythingChanged;
@@ -209,7 +294,8 @@ void HWCleanupPass::runOnProceduralRegion(Region &region) {
   Block &body = region.front();
 
   Operation *lastSideEffectingOp = nullptr;
-  for (Operation &op : llvm::make_early_inc_range(body)) {
+  for (auto it = body.begin(), end = body.end(); it != end; it++) {
+    Operation &op = *it;
     // Merge procedural ifdefs with neighbors in the procedural region.
     if (auto ifdef = dyn_cast<sv::IfDefProceduralOp>(op)) {
       if (auto prevIfDef =
@@ -222,13 +308,15 @@ void HWCleanupPass::runOnProceduralRegion(Region &region) {
       }
     }
 
-    // Merge 'if' operations with the same condition.
+    // Merge 'if' operations.
     if (auto ifop = dyn_cast<sv::IfOp>(op)) {
       if (auto prevIf = dyn_cast_or_null<sv::IfOp>(lastSideEffectingOp)) {
-        if (ifop.cond() == prevIf.cond()) {
-          // We know that there are no side effective operations between the
-          // two, so merge the first one into this one.
-          mergeOperationsIntoFrom(ifop, prevIf);
+        auto newIfOp = mergeIfOps(ifop, prevIf);
+        // If new operation is returned, we have to update the iterator.
+        if (newIfOp != ifop) {
+          lastSideEffectingOp = newIfOp;
+          it = newIfOp->getIterator();
+          continue;
         }
       }
     }
@@ -243,6 +331,87 @@ void HWCleanupPass::runOnProceduralRegion(Region &region) {
     if (op.getNumRegions() != 0)
       runOnRegionsInOp(op);
   }
+}
+
+// This function merge two if operations when they have a common condition.
+// For example,
+// prevIfOp:
+//   if a1 & a2 &... & an & c1 & c2 .. & cn {e1}
+// ifOp:
+//   if b1 & b2 &... & bn & c1 & c2 .. & cn {e2}
+// ====>
+//   if c1 & c2 .. & cn {
+//     if a1 & a2 & ... & an {e1}
+//     if b1 & b2 & ... & bn {e2}
+//   }
+sv::IfOp HWCleanupPass::mergeIfOpConditions(sv::IfOp ifOp, sv::IfOp prevIfOp) {
+
+  ConditionPairInformation condPairInfo(ifOp.cond(), prevIfOp.cond());
+
+  // If there is nothing in common, we cannot merge.
+  if (condPairInfo.commonConj.empty())
+    return ifOp;
+
+  // If both conjunction1 and conjunction2 are empty, it means the conditions
+  // are actually equivalent.
+  if (condPairInfo.conj1.empty() && condPairInfo.conj2.empty()) {
+    mergeOperationsIntoFrom(ifOp, prevIfOp);
+    return ifOp;
+  }
+
+  OpBuilder builder(ifOp.getContext());
+  auto i1Type = ifOp.cond().getType();
+
+  auto generateCondValue =
+      [&](Location loc, llvm::SmallSetVector<Value, 4> &conjunction) -> Value {
+    if (conjunction.empty())
+      return builder.create<hw::ConstantOp>(loc, i1Type, 1);
+
+    return builder.createOrFold<comb::AndOp>(loc, i1Type,
+                                             conjunction.takeVector());
+  };
+
+  // If conj2 is empty, it means we can move ifOp to the block of prevIfOp.
+  if (condPairInfo.conj2.empty()) {
+    // Move ifOp to the end of prevIfOp's then block.
+    builder.setInsertionPointToEnd(prevIfOp.getThenBlock());
+    auto newCond1 = generateCondValue(ifOp.cond().getLoc(), condPairInfo.conj1);
+    ifOp.setOperand(newCond1);
+    // op1 might contain ops defined between op2 and op1, we have to move op2
+    // to the position of op1 to ensure that the dominance doesn't break.
+    prevIfOp->moveBefore(ifOp);
+    ifOp->moveBefore(prevIfOp.getThenBlock(), prevIfOp.getThenBlock()->end());
+    condPairInfo.cleanUpUses();
+    return prevIfOp;
+  }
+
+  // If conj1 is empty, it means we can move prevIfOp to the block of ifOp.
+  if (condPairInfo.conj1.empty()) {
+    // Move prevIfOp to the start of ifOp's then block.
+    builder.setInsertionPointToStart(ifOp.getThenBlock());
+    auto newCond2 =
+        generateCondValue(prevIfOp.cond().getLoc(), condPairInfo.conj2);
+    prevIfOp.setOperand(newCond2);
+    prevIfOp->moveAfter(ifOp.getThenBlock(), ifOp.getThenBlock()->begin());
+    condPairInfo.cleanUpUses();
+    return ifOp;
+  }
+
+  // Ok, now we create a new if op to contain ifOp and prevIfOp.
+  builder.setInsertionPoint(ifOp);
+  auto newCond1 = generateCondValue(ifOp.getLoc(), condPairInfo.conj1);
+  auto newCond2 = generateCondValue(prevIfOp.getLoc(), condPairInfo.conj2);
+  auto cond =
+      builder.createOrFold<comb::AndOp>(ifOp.getLoc(), condPairInfo.commonConj);
+  auto newIf = builder.create<sv::IfOp>(prevIfOp.getLoc(), cond, [&]() {});
+
+  prevIfOp->moveBefore(newIf.getThenBlock(), newIf.getThenBlock()->begin());
+  ifOp->moveAfter(prevIfOp);
+  ifOp.setOperand(newCond1);
+  prevIfOp.setOperand(newCond2);
+  condPairInfo.cleanUpUses();
+
+  return newIf;
 }
 
 std::unique_ptr<Pass> circt::sv::createHWCleanupPass() {
